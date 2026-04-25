6a5dc3dc8cb0e3617770a73980bab6c06369d9e7
omarlinp <omarlinp@gmail.com>
2026-04-25 05:54:33 -0400

    added lesson 1 code

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('hello');
+});
+
+const port = 3000
+
+app.listen(process.env.port || port);
+console.log('Web server is listening at port ' + (process.env.port || port));
