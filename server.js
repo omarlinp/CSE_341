96542aef1217b0961aa93834e8738c8cffa92cdc
omarlinp <omarlinp@gmail.com>
2026-05-13 22:13:13 -0400

    made some small changes to the listen method

--- server.js
+++ server.js
@@ -4,7 +4,5 @@ const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+app.listen(port,()=> console.log(`Server is running on port ${port}`));
 
