6c64da8eb89a8382beb3dd60540caaee02b8cc4e
omarlinp <omarlinp@gmail.com>
2026-05-15 09:00:06 -0400

    added the routes

--- routes/contacts.js
+++ routes/contacts.js
@@ -6,4 +6,6 @@ const controller = require('../controllers/db');
 router.get('/', controller.getdata);
 router.get('/:id', controller.getContactData);
 router.post('/', controller.createContact);
+router.put('/:id', controller.updateContact);
+router.delete('/:id', controller.deleteContact);
 module.exports = router;
