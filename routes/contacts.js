8f99a3c5f8e5f1411ab78c8507a4f72fab97daa8
omarlinp <omarlinp@gmail.com>
2026-05-15 08:43:11 -0400

    added the route for the post request

--- routes/contacts.js
+++ routes/contacts.js
@@ -5,4 +5,5 @@ const controller = require('../controllers/db');
 
 router.get('/', controller.getdata);
 router.get('/:id', controller.getContactData);
+router.post('/post', controller.createContact);
 module.exports = router;
